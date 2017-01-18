20410d7261aa07321a5e36de9279c921bf1394d9
Kat Hicks <kmhicks92@gmail.com>
2017-01-18 14:17:34 +0000

    Refactored first feature test

--- spec/features/playernames_spec.rb
+++ spec/features/playernames_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 RSpec.feature "players start a fight by entering their names and seeing them", :type => :feature do
 
   scenario "user enters name and sees it appear on screen" do
-    visit "/"
+    visit("/")
 
-    fill_in "Player 1", :with => "Kat"
-    fill_in "Player 2", :with => "Bob"
+    fill_in "Player 1", with: "Kat"
+    fill_in "Player 2", with: "Bob"
     click_button "Submit"
 
     expect(page).to have_text("Kat vs. Bob")
