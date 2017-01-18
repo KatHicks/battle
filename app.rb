f81df28919831f9ee8bbb2cf408c8e8639c71aa3
Kat Hicks <kmhicks92@gmail.com>
2017-01-18 14:15:27 +0000

    Wrote my first feature test using rspec-capybara and passed by setting two new routes in the app file. Also set up formatting and colour for rspec output.

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
   set :sessions, true
 
   get '/' do
-    'Hello world!'
+    erb(:index)
   end
+
+  post '/names' do
+    @player_one = params["Player 1"]
+    @player_two = params["Player 2"]
+    erb(:play)
+  end
+
 end
---
+++ spec/features/playernames_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.feature "players start a fight by entering their names and seeing them", :type => :feature do
+
+  scenario "user enters name and sees it appear on screen" do
+    visit "/"
+
+    fill_in "Player 1", :with => "Kat"
+    fill_in "Player 2", :with => "Bob"
+    click_button "Submit"
+
+    expect(page).to have_text("Kat vs. Bob")
+  end
+
+end
