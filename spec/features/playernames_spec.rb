7efe4b1964e26bb2eea031b408545dde58d0959c
Kat Hicks <kmhicks92@gmail.com>
2017-01-19 14:37:00 +0000

    Extended tests using selector syntax

--- spec/features/playernames_spec.rb
+++ spec/features/playernames_spec.rb
@@ -1,20 +1,37 @@
 require 'spec_helper'
 
-RSpec.feature "players start a fight by entering their names and seeing them", :type => :feature do
+feature "players start a fight by entering their names and seeing them", :type => :feature do
 
   scenario "user enters name and sees it appear on screen" do
     sign_in_and_play
     expect(page).to have_content("Kat vs. Bob")
   end
 
-  scenario "player one can see the hit points of player two" do
+end
+
+feature "players can see each other's hit points" do
+
+  scenario "hit points are visible" do
     sign_in_and_play
-    within("div#player_two") do
-      expect(page).to have_content("HP")
-    end
+    expect(page).to have_selector("//p", id: "hit_points_id")
   end
 
-  scenario "player one can attack player two and get a confirmation" do
+  scenario "player one can see the phrase 'HP'" do
+    # same test as above but with alternative syntax
+    sign_in_and_play
+    within("div#player_two_id") { expect(page).to have_content("HP") }
+  end
+
+end
+
+feature "players can attack each other and get a confirmation" do
+
+  scenario "attack button is available" do
+    sign_in_and_play
+    expect(page).to have_selector("//input", id: "attack_button_id")
+  end
+
+  scenario "player gets confirmation after attack" do
     sign_in_and_play
     click_button "Attack"
     expect(page).to have_content("Successful attack!")
